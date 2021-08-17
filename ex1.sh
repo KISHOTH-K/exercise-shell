897a4dc7756043cb2e48f3e2e650de2f1fe8f1a1
KISHOTH-K <73494038+KISHOTH-K@users.noreply.github.com>
2021-08-17 22:38:13 +0530

    Add files via upload

---
+++ ex1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+arr1=(Saturday Sunday Monday Tuesday Wednesday)
+echo ${arr1[3]}
+
+
+RESULT:
+Tuesday
---
+++ ex3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+a=15
+b=20
+c=$a
+b=$c
+echo "value of b is:$b"
+echo "value of c is:$c"
+
+
+RESULT:
+value of b is:15
+value of c is:15
